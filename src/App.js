02ca81fee071f54412bee5db89f90051a9904b6d
thierno <thiernoibrahimalo@esp.sn>
2021-02-02 12:30:53 +0000

    dockerizing all

--- src/App.js
+++ src/App.js
@@ -4,40 +4,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import 'bootswatch/dist/litera/bootstrap.min.css'; // Added this :boom:\
 import './component/style.css'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {  } from 'react-router-dom';
 //pages
-import Header from './component/Header.jsx';
-import Home from './component/Home.jsx';
-import Login from './component/Login.jsx';
-import Depot from './component/Depot.jsx';
-import NotFound from './component/NotFound.jsx';
-import Profile from './component/Profile.jsx';
-import Facture from './component/Facture.jsx';
-import Client from './component/Client.jsx';
-import Fournisseur from './component/Fournisseur.jsx';
-import test from './component/test.jsx';
+// import Header from './component/Header.jsx';
+// import Home from './component/Home.jsx';
+// import Login from './component/Login.jsx';
+// import NotFound from './component/NotFound.jsx';
+// import Profile from './component/Profile.jsx';
 function App() {
   return (
     <div className="App ">
-      <Router>
+      {/* <Router>
       <Header/>
         <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact ="/login" component={Login}/>
           <Route path="/d" component={Header} />
-          <Route path="/Dette Clients" component={Client} />
-          <Route path="/tests" component={test}/>
-          <Route path="/depots" component={Depot} />
           <Route path="/profile" component={Profile} />
-          <Route path="/facture" component={Facture} /> 
-          <Route path="/Dette Clients" component={Client} />
-          <Route path="/Dette Fournisseur" component={Fournisseur} />
           <Route path="*" component={NotFound} />
 
-         <Route path="/fournisseurs " component={Fournisseur} /> 
 {/*  */}
-      </Switch>
-    </Router>
+      {/* </Switch> */}
+      {/* </Router> */} 
     </div>
   );
 }
